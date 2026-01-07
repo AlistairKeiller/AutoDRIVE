fedc6b987934f6432c6bbaa2034cf9338af33dfd
Alistair Keiller <alistair@keiller.net>
2026-01-07 11:11:42 -0800

    precompute distances

--- Assets/lidarml.cs
+++ Assets/lidarml.cs
@@ -10,55 +10,76 @@ public class LidarML : MonoBehaviour
     [SerializeField, Range(0f, 360f)] float theta = 270f;
     [SerializeField, Range(0.1f, 500f)] float range = 50f;
     [SerializeField] LayerMask layers = Physics.DefaultRaycastLayers;
+
     float[] distances;
+    Vector3[] directions;
 
-    float timer;
-
-    void Awake() => distances = new float[samples];
-
-    void Update()
+    void Awake()
     {
-        CaptureScan();
-        StartCoroutine(SendScan());
+        distances = new float[samples];
+        directions = new Vector3[samples];
+        RebuildDirections();
+        StartCoroutine(SendLoop());
+    }
+
+    void OnValidate()
+    {
+        distances = new float[samples];
+        directions = new Vector3[samples];
+        RebuildDirections();
+    }
+
+    void RebuildDirections()
+    {
+        var forward = Vector3.forward;
+        var start = -theta * 0.5f;
+        var step = samples > 1 ? theta / (samples - 1) : 0f;
+        for (var i = 0; i < samples; i++)
+            directions[i] = Quaternion.Euler(0f, start + step * i, 0f) * forward;
     }
 
     void CaptureScan()
     {
         var pos = transform.position;
-        var forward = transform.forward;
-        var start = -theta * 0.5f;
-        var step = samples > 1 ? theta / (samples - 1) : 0f;
+        var euler = transform.rotation.eulerAngles;
+        var rot = Quaternion.Euler(0f, euler.y, 0f);
         for (var i = 0; i < samples; i++)
         {
-            var dir = Quaternion.Euler(0f, start + step * i, 0f) * forward;
+            var dir = rot * directions[i];
             distances[i] = Physics.Raycast(pos, dir, out var hit, range, layers, QueryTriggerInteraction.Ignore)
                 ? hit.distance
                 : range;
         }
     }
 
-    IEnumerator SendScan()
+    IEnumerator SendLoop()
     {
-        var payload = JsonUtility.ToJson(new Request { command = "publish_lidar", data = distances });
-        var req = new UnityWebRequest(serverUrl, "POST")
+        var json = new Request();
+        while (true)
         {
-            uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(payload)),
-            downloadHandler = new DownloadHandlerBuffer()
-        };
-        req.SetRequestHeader("Content-Type", "application/json");
-        yield return req.SendWebRequest();
+            CaptureScan();
+            json.command = "publish_lidar";
+            json.data = distances;
+            var payload = Encoding.UTF8.GetBytes(JsonUtility.ToJson(json));
+            using var req = new UnityWebRequest(serverUrl, "POST")
+            {
+                uploadHandler = new UploadHandlerRaw(payload),
+                downloadHandler = new DownloadHandlerBuffer()
+            };
+            req.SetRequestHeader("Content-Type", "application/json");
+            yield return req.SendWebRequest();
+        }
     }
 
     void OnDrawGizmosSelected()
     {
         var pos = transform.position;
-        var forward = transform.forward;
-        var start = -theta * 0.5f;
-        var step = samples > 1 ? theta / (samples - 1) : 0f;
+        var euler = transform.rotation.eulerAngles;
+        var rot = Quaternion.Euler(0f, euler.y, 0f);
         Gizmos.color = Color.red;
         for (var i = 0; i < samples; i++)
         {
-            var dir = Quaternion.Euler(0f, start + step * i, 0f) * forward;
+            var dir = rot * directions[i];
             var hitDetected = Physics.Raycast(pos, dir, out var hit, range, layers, QueryTriggerInteraction.Ignore);
             var end = hitDetected ? hit.point : pos + dir * range;
             Gizmos.DrawLine(pos, end);
@@ -67,9 +88,5 @@ public class LidarML : MonoBehaviour
     }
 
     [System.Serializable]
-    class Request
-    {
-        public string command;
-        public float[] data;
-    }
+    class Request { public string command; public float[] data; }
 }
